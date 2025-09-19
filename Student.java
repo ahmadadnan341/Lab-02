fbd0626a1d147d17ef6fd39ce7d943998a7e885b
ahmadadnan341 <161201420+ahmadadnan341@users.noreply.github.com>
2025-09-19 15:50:24 +0500

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,140 @@
+public class Student{
+	
+	private String studentID = "SP25-BCS-";
+	
+	private String name;
+	
+	private String gender;
+	
+	private String dateOfBirth;
+	
+	private String address;
+	
+	private static int registrationNumber = 1;
+	
+	public Student(){
+		
+	}
+	
+	public Student(String studentID, String name, String gender, String dateOfBirth, String address){
+		
+		this.studentID = studentID;
+		
+		this.name = name;
+		
+		this.gender = gender;
+		
+		this.dateOfBirth = dateOfBirth;
+		
+		this.address = address;
+		
+	}
+	
+	public Student(String name, String gender, String dateOfBirth, String address){
+		
+		this.studentID = studentID + registrationNumber++;
+		
+		this.name = name;
+		
+		this.gender = gender;
+		
+		this.dateOfBirth = dateOfBirth;
+		
+		this.address = address;
+		
+	}
+	
+	public Student(Student s){
+		
+		this.studentID = s.studentID;
+		
+		this.name = s.name;
+		
+		this.gender = s.gender;
+		
+		this.dateOfBirth = s.dateOfBirth;
+		
+		this.address = s.address;
+		
+	}
+		
+	public void setStudentID(String studentID){
+		
+		this.studentID = studentID;
+		
+	}
+	
+	public String getStudentID(){
+		
+		return studentID;
+		
+	}
+	
+	public void setName(String name){
+		
+		this.name = name;
+		
+	}
+	
+	public String getName(){
+		
+		return name;
+		
+	}
+	
+	public void setDateOfBirth(String dateOfBirth){
+		
+		this.dateOfBirth = dateOfBirth;
+		
+	}
+	
+	public String getDateOfBirth(){
+		
+		return dateOfBirth;
+		
+	}
+	
+	public void setGender(String gender){
+		
+		this.gender = gender;
+		
+	}
+	
+	public String getGender(){
+		
+		return gender;
+		
+	}
+	
+	public void setAddress(String address){
+		
+		this.address = address;
+		
+	}
+	
+	public String getAddress(){
+		
+		return address;
+		
+	}
+	
+	public static void setRegistrationNumber(int regNum){
+		
+		registrationNumber = regNum;
+		
+	}
+	
+	public static int getRegistrationNumber(){
+		
+		return registrationNumber;
+		
+	}
+	
+	@Override
+	public String toString(){
+		
+		return String.format("Student ID: %s\r\nStudent name: %s\r\nStudent gender: %s\r\nDate of birth: %s\r\nStudent address: %s\r\n", studentID, name, gender, dateOfBirth, address);
+		
+	}
+	
+}
